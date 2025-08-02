a398401dc0e2ec0f1be4b4df387e87cc0199cd1a
dylee00 <dylee6820@gmail.com>
2025-08-02 17:15:08 +0900

    Chore: 프로젝트 초기 설정

---
+++ src/test/java/Codify/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package Codify.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
